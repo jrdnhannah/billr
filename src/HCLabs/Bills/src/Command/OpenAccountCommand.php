c25f0d208154281a4c8776589b0218f2a4fc1243
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-09-27 22:03:21 +0100

    [Open Account Command] Use constructor instead of public properties, with getters

--- src/HCLabs/Bills/src/Command/OpenAccountCommand.php
+++ src/HCLabs/Bills/src/Command/OpenAccountCommand.php
@@ -2,20 +2,78 @@
 
 namespace HCLabs\Bills\Command;
 
+use HCLabs\Bills\Model\Service;
+use HCLabs\Bills\Value\BillingPeriod;
+use HCLabs\Bills\Value\Money;
+
 class OpenAccountCommand
 {
     /** @var \HCLabs\Bills\Model\Service */
-    public $service;
+    private $service;
 
     /** @var string */
-    public $accountNumber;
+    private $accountNumber;
 
-    /** @var double */
-    public $recurringCharge;
+    /** @var Money */
+    private $recurringCharge;
 
     /** @var \DateTime */
-    public $dateOpened;
+    private $dateOpened;
 
-    /** @var \DateInterval */
-    public $billingPeriod;
+    /** @var BillingPeriod */
+    private $billingPeriod;
+
+    public function __construct(
+        Service $service,
+        $accountNumber,
+        Money $recurringCharge,
+        \DateTime $dateOpened,
+        BillingPeriod $billingPeriod
+    ) {
+        $this->service = $service;
+        $this->accountNumber = $accountNumber;
+        $this->recurringCharge = $recurringCharge;
+        $this->dateOpened = $dateOpened;
+        $this->billingPeriod = $billingPeriod;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccountNumber()
+    {
+        return $this->accountNumber;
+    }
+
+    /**
+     * @return BillingPeriod
+     */
+    public function getBillingPeriod()
+    {
+        return $this->billingPeriod;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateOpened()
+    {
+        return $this->dateOpened;
+    }
+
+    /**
+     * @return Money
+     */
+    public function getRecurringCharge()
+    {
+        return $this->recurringCharge;
+    }
+
+    /**
+     * @return Service
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
 }
