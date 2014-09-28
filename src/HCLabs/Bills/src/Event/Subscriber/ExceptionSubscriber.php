59c579d3f3a62759fe178400e6ba195a9f7865b6
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-09-28 13:29:25 +0100

    [Exception] Exception subscriber to catch HTTP Exceptions for XHR's, as to return a JSON parsable error response

---
+++ src/HCLabs/Bills/src/Event/Subscriber/ExceptionSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HCLabs\Bills\Event\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => [
+                ['onException', 200]
+            ]
+        ];
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $e
+     */
+    public function onException(GetResponseForExceptionEvent $e)
+    {
+        $exception = $e->getException();
+        if ($e->getRequest()->isXmlHttpRequest() && $exception instanceof HttpException) {
+            $e->setResponse(
+                new JsonResponse([
+                        'status_code' => $exception->getStatusCode(),
+                        'message'     => $exception->getMessage()
+                    ]
+                )
+            );
+        }
+    }
+
+}
