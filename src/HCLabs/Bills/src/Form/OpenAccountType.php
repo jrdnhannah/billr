22bc32f408b211c3743bbfaa3b53d1ac6aa160cc
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-10-25 21:11:55 +0100

    [OpenAccountType] Use new form types, tell form how to construct command

--- src/HCLabs/Bills/src/Form/OpenAccountType.php
+++ src/HCLabs/Bills/src/Form/OpenAccountType.php
@@ -2,8 +2,11 @@
 
 namespace HCLabs\Bills\Form;
 
+use HCLabs\Bills\Command\Scenario\OpenAccount\OpenAccountCommand;
+use HCLabs\Bills\Value\BillingPeriod;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class OpenAccountType extends AbstractType
@@ -13,15 +16,16 @@ class OpenAccountType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('service', 'entity', ['class' => 'HCLabs\Bills\Model\Service'])
-                ->add('accountNumber', 'text')
-                ->add('recurringCharge', 'number')
-                ->add('dateOpened', 'datetime')
+        $builder->add('service', 'entity', ['class' => 'HCLabs\Bills\Model\Service', 'mapped' => false])
+                ->add('accountNumber', 'account_number', ['mapped' => false, 'display_label' => 'Account Number'])
+                ->add('recurringCharge', 'money_type', ['mapped' => false, 'label' => false, 'display_label' => 'Recurring Charge'])
+                ->add('dateOpened', 'datetime', ['mapped' => false])
                 ->add('billingPeriod', 'choice', [
                         'choices' => [
                             'P1M' => 'Monthly',
                             'P3M' => 'Quarterly'
-                        ]
+                        ],
+                        'mapped' => false
                     ]
                 )
                 ->add('Open', 'submit');
@@ -33,7 +37,15 @@ class OpenAccountType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults([
-                'data_class' => 'HCLabs\Bills\Command\OpenAccountCommand',
+                'empty_data' => function (FormInterface $form) {
+                    return new OpenAccountCommand(
+                        $form->get('service')->getData(),
+                        $form->get('accountNumber')->getData(),
+                        $form->get('recurringCharge')->getData(),
+                        $form->get('dateOpened')->getData(),
+                        new BillingPeriod($form->get('billingPeriod')->getData())
+                    );
+                }
             ]
         );
     }
